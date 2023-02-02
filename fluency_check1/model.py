e631dad71e401b242af5cefdcd3327476e8e56c0
Omkar Ukirde <ukirdeomkar4@gmail.com>
2023-02-02 23:55:05 +0530

    api output generated but using html post request

---
+++ fluency_check1/model.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+class fluent(BaseModel):
+    n_mfcc0: float
+    n_mfcc1: float
+    n_mfcc2: float
+    n_mfcc3: float
+    n_mfcc4: float
+    n_mfcc5: float
+    n_mfcc6: float
+    n_mfcc7: float
+    n_mfcc8: float
+    n_mfcc9: float
+    n_mfcc10: float
+    n_mfcc11: float
+    n_mfcc12: float
+    n_mfcc13: float
+    n_mfcc14: float
+    n_mfcc15: float
+    n_mfcc16: float
+    n_mfcc17: float
+    n_mfcc18: float
+    n_mfcc19: float
+    rmse: float
+    spectral_flux: float
+    zcr: float
