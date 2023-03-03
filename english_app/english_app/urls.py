c28b98bb8ae5c49b7ee4082e2a93b2741c1accb8
Tejas Veer <tejasveer41@gmail.com>
2023-03-03 16:33:24 +0530

    Revert "new"
    This reverts commit c7acc1fef492fa11c6726125dc09c869bf37e673.

--- english_app/english_app/urls.py
+++ english_app/english_app/urls.py
@@ -4,6 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
-    path('users/', include('users_app.urls')),
+    path('', include('authapp.urls'))
 ]
